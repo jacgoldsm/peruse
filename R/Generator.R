75a1ec47e866e3bdcee71e3820f547ec5f704bd0
Jacob Goldsmith <jacobg314@hotmail.com>
2020-10-02 16:15:15 -0700

    commit

--- R/Generator.R
+++ R/Generator.R
@@ -9,22 +9,23 @@
 #' #Fibonacci sequence: Each time yield_next is called, the next
 #' #number from the sequence will be generated and returned
 #' expr <- "if (i == 0) {
-#'             n0 <- 0;
-#'            n1 <- 0;
-#'            n <- n0 + n1; };
+#'             n0 <- 0
+#'            n1 <- 0
+#'            n <- n0 + n1
+#'             }
 #'         if (i == 1) {
-#'            n0 <- 0; n1 <- 1;
-#'            n <- n0 + n1;
-#'         };
+#'            n0 <- 0; n1 <- 1
+#'            n <- n0 + n1
+#'         }
 #'         if (i > 1) {
 #'
-#'            n <- n0 + n1;
-#'            a <- n1;
-#'             b <- n0 + n1;
-#'             n0 <- a;
-#'             n1 <- b;
-#'           };
-#'           i <- i + 1;"
+#'            n <- n0 + n1
+#'            a <- n1
+#'             b <- n0 + n1
+#'             n0 <- a
+#'             n1 <- b
+#'           }
+#'           i <- i + 1"
 #'
 #' gen <- Generator(result = expr,
 #'                 current = c(i = 0,
