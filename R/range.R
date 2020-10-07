b240fa73e147199cbd51daa306f8158c19ab1eb3
Jacob Goldsmith <jacobg314@hotmail.com>
2020-10-07 10:28:15 -0700

    range changes

--- R/range.R
+++ R/range.R
@@ -1,14 +1,18 @@
-#' Create a Python-style range vector
+#' Python-style range function
+#'
+#' Wrapper around base::seq that replaces the maximal end value with the supremum
+#' and returns an empty vector if b <= a, in the style of Python's range().
+#' Note that itertools::range views end as a supremum, not a maximum, thus range(a,b)
+#' is equivalent to the set `[`a,b) when a < b or `{}` when b >= a.
 #'
 #' @param a minimum, integer
 #' @param b supremum, integer
+#' @param ... other params passed to base::seq()
 #' @examples
 #' range(1,5)
 #' range(9,10)
 #' @export
 
-range <- function(a,b) {
-  a <- as.integer(a)
-  b <- as.integer(b)
-  if (a < b) return(a:(b-1)) else return(numeric())
+range <- function(a,b, ...) {
+  if (a < b) return(seq(a, b-1,...)) else return(numeric())
 }
